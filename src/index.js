22681c31dfb763290de82ff62bcbde641d6604e1
proczniak <proczniak@gmail.com>
2018-01-25 09:10:14 +0100

    pulling actual data from youtube works. Also it's properly displayed

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyAcA0mHYYTcHOy2FVUztX8MwiX56AHK6Iw';
 
@@ -10,7 +11,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = { videos: [] };
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'slayer'}, (videos) => {
       this.setState({videos});
     });
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
